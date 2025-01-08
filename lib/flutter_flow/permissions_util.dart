ada438671957a74f4fba3bf5e5ea830f69503a42
benessuti <benessuti@gmail.com>
2025-01-08 14:27:59 -0300

    Update permissions_util.dart

--- lib/flutter_flow/permissions_util.dart
+++ lib/flutter_flow/permissions_util.dart
@@ -2,7 +2,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 import '/flutter_flow/flutter_flow_util.dart';
-import '/backend/push_notifications/push_notifications_util.dart';
+// import '/backend/push_notifications/push_notifications_util.dart';
 
 const kPermissionStateToBool = {
   PermissionStatus.granted: true,
